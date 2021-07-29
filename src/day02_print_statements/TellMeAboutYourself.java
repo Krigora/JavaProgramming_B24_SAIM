6673583b69261384777c6ae4dbee728741636887
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-07-28 21:49:26 -0500

    Day 2 + Project set up

---
+++ src/day02_print_statements/TellMeAboutYourself.java
@@ -0,0 +1,13 @@
+package day02_print_statements;
+
+public class TellMeAboutYourself {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello my name is Saim");
+        System.out.println("I live in Chicago");
+        System.out.println("I like to code java");
+
+    }
+
+}
