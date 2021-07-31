3b6e61874f127fab2959a6200d3f61ea94d944dd
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-07-31 13:06:23 -0500

    Day 4

---
+++ src/day03_comments_escape_sequence/CommentExamples.java
@@ -0,0 +1,41 @@
+package day03_comments_escape_sequence;
+
+public class CommentExamples {
+
+    public static void main(String[] args) {
+
+        // single line comment
+
+
+        // This statement prints 'Hello World' to the console
+
+        System.out.println("Hello World"); // prints
+
+    // problem:
+//        System.out.println(bye world);
+
+        // Select everything command/control + /
+
+
+    /* open multi line
+
+        Here line1
+        line 2
+        line 3
+
+        close multi line  */
+
+        /**
+         *
+         * Java docs comments
+         *
+         */
+
+
+        //TODO: Study, sleep, finsh method tomorrow
+
+
+    }
+
+
+}
