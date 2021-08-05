3149e402626c4f11438cb512c6fab2d4b4570aad
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-08-05 18:59:57 -0500

    Day 8 operators

--- src/day08_relational_logical_operators/OperatorsPractice.java
+++ src/day08_relational_logical_operators/OperatorsPractice.java
@@ -37,6 +37,30 @@ public class OperatorsPractice {
         System.out.println("c: " + c);
         System.out.println("d: " + d);
 
+        System.out.println();
+
+        int t = 100; // t= 100 | 101 | 100 | 101 | 100
+        int p = - ++t * -t-- / t++ + --t;
+        //   - 101 * - 101 / 100 + 100
+        //    10201 / 100 + 100
+        //    102 + 100
+        //    202
+
+        System.out.println("t: " + t);
+        System.out.println("p: " + p);
+
+        System.out.println();
+        int R = 20; // R = 20 | 19 | 20 | 19 | 18
+        int W = -R-- + -R++ + --R * R-- % 2;
+        //      - 20 + - 19 + 19 * 19 % 2
+        //      - 20 - 19 + 19 * 19 % 2
+        //      - 20 - 19 + 361 % 2 -> 361 / 2 -> 180 * 2 = 360 | 361 - 360 = 1
+        //      - 20 - 19 + 1
+        //      -39 + 1
+        //      -38
+
+        System.out.println("R: " + R);
+        System.out.println("W: " + W);
 
 
     }
