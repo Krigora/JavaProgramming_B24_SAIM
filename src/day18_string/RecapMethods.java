5050e6b87b222cf3dbadfd9ddcdbd9625edf9df1
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-08-19 19:28:27 -0500

    Office hours Problem set 1

--- src/day18_string/RecapMethods.java
+++ src/day18_string/RecapMethods.java
@@ -16,11 +16,17 @@ public class RecapMethods {
         System.out.println(day.toLowerCase());
         System.out.println(day);
 
-        System.out.println(day.length() + 1);
-        System.out.println(day);
+        System.out.println(day.length() + 1);  // 8 (int) + 1 (int) -> 9
+        System.out.println(day); // THURSDAY
 
-        day = day.length() + "";
-        System.out.println(day + 2 );
+        // day = day.length(); length method returns int and we are trying to store to String
+        day = day.length() + ""; // types: int + String -> concatenation -> String type back // values of "8"
+        System.out.println(day + 2); // String + int -> concatenation -> "8" + 2 -> "82"
+
+        System.out.println("5 + 2 = " + 5 + 2);
+        //  "5 + 2 = " + 5 -> "5 + 2 = 5" // String + int
+        //  "5 + 2 = 5"  + 2 -> 5 + 2 = 52
+        System.out.println("5 + 2 = " + (5 + 2) );
 
     }
 }
