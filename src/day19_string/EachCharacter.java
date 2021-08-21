36a51d0716e6e320fdcd59d0aced2e2f80077313
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-08-21 10:26:32 -0500

    Day 19 String

--- src/day19_string/EachCharacter.java
+++ src/day19_string/EachCharacter.java
@@ -8,15 +8,22 @@ public class EachCharacter {
 
         // charAt(number) gives a char based on the number you give
 
-        System.out.println(java.charAt(0));
-        System.out.println(java.charAt(1));
-        System.out.println(java.charAt(2));
+        System.out.print(java.charAt(0));
+        System.out.print(java.charAt(1));
+        System.out.print(java.charAt(2));
         System.out.println(java.charAt(3));
 
         //System.out.println(java.charAt(100));
         //  If you use an index number that does not exist in your String it will go out of bounds
 
-        // come back at 11:15 est
+        char firstLetter = java.charAt(0);
+
+        System.out.println(java.charAt(0) + java.charAt(1) + java.charAt(2) + java.charAt(3));
+        // Above will add the ascii values of the char and print it. char + char
+
+        System.out.println("" + java.charAt(0) + java.charAt(1) + java.charAt(2) + java.charAt(3));
+        // by adding the empty space we can do concatenation and get the character values, not addition. String + char
+
 
     }
 }
