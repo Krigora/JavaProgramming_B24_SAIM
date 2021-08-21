afee9352b7f92d3b97cf8a7f5b12788c9309fdc9
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-08-21 10:00:38 -0500

    Day 19 String

--- src/day19_string/EachCharacter.java
+++ src/day19_string/EachCharacter.java
@@ -1,4 +1,22 @@
 package day19_string;
 
 public class EachCharacter {
+    public static void main(String[] args) {
+
+        String java = "javaa";
+        //             01234
+
+        // charAt(number) gives a char based on the number you give
+
+        System.out.println(java.charAt(0));
+        System.out.println(java.charAt(1));
+        System.out.println(java.charAt(2));
+        System.out.println(java.charAt(3));
+
+        //System.out.println(java.charAt(100));
+        //  If you use an index number that does not exist in your String it will go out of bounds
+
+        // come back at 11:15 est
+
+    }
 }
