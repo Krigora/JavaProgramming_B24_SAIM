331412a5726609b68ccfa0e9ac6755849d6b440c
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-08-25 18:58:47 -0500

    Day 22 Loops

--- src/day22_loops/MinAndMax.java
+++ src/day22_loops/MinAndMax.java
@@ -13,8 +13,10 @@ public class MinAndMax {
         Scanner input = new Scanner(System.in);
 
         int counter = 1;
-        int max = -2147483648;
+        int max = -2147483647; // 0   -2147483648
         int min = 2147483647;
+
+        // 5
 
         while(counter <= 5){
             counter++;
@@ -22,7 +24,18 @@ public class MinAndMax {
             System.out.println("Enter a number:");
             int number = input.nextInt();
 
+            if(number > max){  // 4
+                max = number;
+            }
+
+            if(number < min){
+                min = number;
+            }
+
         }
+
+        System.out.println("Max number: " + max);
+        System.out.println("Min number: " + min);
 
 
     }
