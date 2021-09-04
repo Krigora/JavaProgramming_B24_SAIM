343361fa15ae55c869a458e5132a2b6bbe56b19a
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-04 13:31:26 -0500

    Day 29 Arrays

--- src/day29_array/PickMonth.java
+++ src/day29_array/PickMonth.java
@@ -1,5 +1,7 @@
 package day29_array;
 
+import java.util.Scanner;
+
 public class PickMonth {
 
     /*
@@ -12,10 +14,16 @@ public class PickMonth {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
         String [] months = {
-                "Jan", "Feb", "Mar", "Apr" , "May", "Jun", "Jul",
-                "Aug", "Sep", "Oct", "Nov", "Dec"
-        };
+               "Jan", "Feb", "Mar", "Apr" , "May", "Jun", "Jul",
+                "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+        System.out.println("Enter a month number: ");
+        int num = input.nextInt();
+
+        System.out.println("The month you picked is: " +  months[num - 1] ); // allows us to go from a number to an index
 
     }
 }
