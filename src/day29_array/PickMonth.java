3e5c467e4a1d327921a57f4b2a1a160fb47f9be5
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-04 13:16:54 -0500

    Day 29 Arrays

---
+++ src/day29_array/PickMonth.java
@@ -0,0 +1,21 @@
+package day29_array;
+
+public class PickMonth {
+
+    /*
+        Ask the user to enter a month number
+        Print which month they select
+        1 -> January
+        ..
+        12 -> december
+     */
+
+    public static void main(String[] args) {
+
+        String [] months = {
+                "Jan", "Feb", "Mar", "Apr" , "May", "Jun", "Jul",
+                "Aug", "Sep", "Oct", "Nov", "Dec"
+        };
+
+    }
+}
