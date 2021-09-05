0679f2a5a52bf2a3fa399ec6acfb6057fec8b2d5
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-05 12:00:46 -0500

    Day 30 Arrays

---
+++ src/day30_array/EvenAndOdd.java
@@ -0,0 +1,52 @@
+package day30_array;
+
+import java.util.Arrays;
+
+public class EvenAndOdd {
+
+    /*
+
+        declare an int array
+        count how many even number there is
+        count how many odd numbers there is
+
+        print your counters
+
+        challenges:
+            traditional for loop
+            for each loop
+
+            - not hard code in initial array, but use scanner to get the numbers
+
+     */
+
+    public static void main(String[] args) {
+
+        int [] numbers = {9, 24, 5, 1, 5, 29, 52};
+        int even = 0;
+        int odd = 0;
+
+        String evenNumbers = "";
+        String oddNumbers = "";
+
+        for(int i = 0; i < numbers.length; i++){
+
+            if(numbers[i] % 2 == 0){
+                even++;
+                evenNumbers += numbers[i] + " ";
+            } else {
+                odd++;
+                oddNumbers += numbers[i] + " ";
+            }
+
+        }
+
+        System.out.println(Arrays.toString(numbers));
+        System.out.println("Even: " + even);
+        System.out.println("Even numbers: " + evenNumbers);
+        System.out.println("Odd: " + odd);
+        System.out.println("odd numbers: " + oddNumbers);
+
+
+    }
+}
