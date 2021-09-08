328d4fa513fce6f5645d5a4d448034c1bfbd89ae
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-08 17:17:53 -0500

    Office hour Arrays

--- src/office_hours/practice_09_08_2021/WebsiteChecker.java
+++ src/office_hours/practice_09_08_2021/WebsiteChecker.java
@@ -22,5 +22,38 @@ public class WebsiteChecker {
 
         String [] websites = {"bing.com", "wikimedia.org", "twitter.com", "amazonaws.com", "japanpost.jp", "mysql.com", "nasa.gov", "linkedin.com", "ucla.edu", "github.io", "spotify.com", "mozilla.org", "nba.com", "comcast.net", "tamu.edu", "utexas.edu"};
 
+        int com = 0, org = 0, edu = 0, other = 0;
+
+        /*
+            [ com, org, edu, other ]
+         */
+
+        int [] counter = new int [4];
+
+        for(String eachSite : websites){ // eachSite == websites[i]
+
+            if(eachSite.endsWith(".com")){  // if(websites[i].endsWith(.com))
+                com++;
+                counter[0]++;
+            } else if(eachSite.endsWith(".org")){
+                org++;
+                counter[1]++;
+            } else if(eachSite.endsWith(".edu")){
+                edu++;
+                counter[2]++;
+            } else {
+                other++;
+                counter[3]++;
+            }
+
+        }
+
+        System.out.println(".com: " + com);
+        System.out.println(counter[0]);
+        System.out.println(".org: " + org); // System.out.println(counter[1]);
+        System.out.println(".edu: " + edu); // System.out.println(counter[2]);
+        System.out.println("other: " + other);// System.out.println(counter[3]);
+
+
     }
 }
