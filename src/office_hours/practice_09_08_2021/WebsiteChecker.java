cf5995f1cfd65920666320254b2b039e5623abb2
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-08 16:46:07 -0500

    Office hour Arrays

---
+++ src/office_hours/practice_09_08_2021/WebsiteChecker.java
@@ -0,0 +1,26 @@
+package office_hours.practice_09_08_2021;
+
+public class WebsiteChecker {
+
+    public static void main(String[] args) {
+
+        /*
+
+            traditional for loop: any time you want to use indexes, want to modify any element in the array, if you want to go backwards,
+
+           for each loop: always goes from the first element to the last one, no indexes involved.
+            -> You cannot change elements of the array in this loop
+
+         */
+
+        /*
+        Website checker
+            Given some urls determine how many .com, .org, and .edu websites there are. Also keep track of an ‘other’ category for websites that have a different domain
+            Sample websites for an array:
+            bing.com, wikimedia.org, twitter.com, amazonaws.com, japanpost.jp, mysql.com, nasa.gov, linkedin.com, ucla.edu, github.io, spotify.com, mozilla.org, nba.com, comcast.net, tamu.edu, utexas.edu,
+         */
+
+        String [] websites = {"bing.com", "wikimedia.org", "twitter.com", "amazonaws.com", "japanpost.jp", "mysql.com", "nasa.gov", "linkedin.com", "ucla.edu", "github.io", "spotify.com", "mozilla.org", "nba.com", "comcast.net", "tamu.edu", "utexas.edu"};
+
+    }
+}
