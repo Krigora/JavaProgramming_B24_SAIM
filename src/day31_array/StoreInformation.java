0461f1465b4272462232ca7b23784d4d8b71d185
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-08 18:41:08 -0500

    Day 31 Arrays

--- src/day31_array/StoreInformation.java
+++ src/day31_array/StoreInformation.java
@@ -3,7 +3,7 @@ package day31_array;
 public class StoreInformation {
     public static void main(String[] args) {
 
-        String [] items = { "Shoes", "Gloves", "AirPod", "Ipod", "Backpack" };
+        String [] items = { "Shoes", "Jackets", "Gloves", "AirPod", "Ipod", "Backpack" };
         double [] prices = { 89.99, 150.0, 9.99, 250.0, 439.5, 39.99 };
         int [] itemIds = { 12345, 12346, 12347, 12348, 12349, 12350 };
 
@@ -24,7 +24,13 @@ public class StoreInformation {
             System.out.println("No sorry, we don't have jackets");
         }
 
-        //
+        // Printing all the information
+        // item id | name | price
+
+        for(int i = 0; i < items.length; i++){
+            System.out.println(itemIds[i] + " | " + items[i] + " | $" + prices[i]);
+        }
+
 
     }
 }
