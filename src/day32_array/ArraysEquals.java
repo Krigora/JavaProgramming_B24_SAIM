328f666438dec887e5b043d5dd642a8853534202
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-09 20:34:59 -0500

    Day 32 Arrays

---
+++ src/day32_array/ArraysEquals.java
@@ -0,0 +1,16 @@
+package day32_array;
+
+import java.util.Arrays;
+
+public class ArraysEquals {
+
+    public static void main(String[] args) {
+
+        int [] a = { 1, 2, 3};
+        int [] b = {1, 2, 3};
+
+        Arrays.equals(a, b);
+
+
+    }
+}
--- src/day32_array/StringSortArray.java
+++ src/day32_array/StringSortArray.java
@@ -1,9 +1,15 @@
 package day32_array;
 
+import java.util.Arrays;
+
 public class StringSortArray {
     public static void main(String[] args) {
 
-        String [] strs = {"java", "8eight", "Hello", "hello", "HEllo", "$dollar"};
+        String [] strs = {"4java", "8eight", "Hello", "hello", "HEllo", "$Dollar", "words5", "$apple", "double$", " space"};
+
+        Arrays.sort(strs);
+
+        System.out.println(Arrays.toString(strs));
 
     }
 }
