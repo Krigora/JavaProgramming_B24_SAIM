2f3de032f31b39f683d7135faad86a25a8edec05
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-09 20:19:03 -0500

    Day 32 Arrays

---
+++ src/day32_array/StringSortArray.java
@@ -0,0 +1,9 @@
+package day32_array;
+
+public class StringSortArray {
+    public static void main(String[] args) {
+
+        String [] strs = {"java", "8eight", "Hello", "hello", "HEllo", "$dollar"};
+
+    }
+}
