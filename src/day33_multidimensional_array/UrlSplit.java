5fd54f713af556a66bb73400675e2621d240abdf
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-11 10:20:46 -0500

    Day 33 2d Arrays

---
+++ src/day33_multidimensional_array/UrlSplit.java
@@ -0,0 +1,19 @@
+package day33_multidimensional_array;
+
+import java.util.Arrays;
+
+public class UrlSplit {
+    public static void main(String[] args) {
+
+        String url = "https://learn.cybertekschool.com/courses/130";
+
+        String [] parts = url.split("/");
+
+        System.out.println(Arrays.toString(parts));
+
+        Arrays.sort(parts);
+
+        System.out.println(Arrays.toString(parts));
+
+    }
+}
