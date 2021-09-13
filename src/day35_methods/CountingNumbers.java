32e6ed3124dc5e969f2f593a0439e491f575923b
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-13 18:34:44 -0500

    Day 35 Methods

---
+++ src/day35_methods/CountingNumbers.java
@@ -0,0 +1,12 @@
+package day35_methods;
+
+public class CountingNumbers {
+
+    /*
+        Ver 1: Define a method that will count number from 0 to 5
+        print the numbers in one line
+
+        Ver 2: Define another method that will count numbers from 0 to a given number (parameter)
+     */
+
+}
