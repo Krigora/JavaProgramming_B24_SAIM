e04bdeae0ba6f87990ca81b95bb955c0ce19060f
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-13 19:02:29 -0500

    Day 35 Methods

--- src/day35_methods/CountingNumbers.java
+++ src/day35_methods/CountingNumbers.java
@@ -9,4 +9,29 @@ public class CountingNumbers {
         Ver 2: Define another method that will count numbers from 0 to a given number (parameter)
      */
 
+    public static void main(String[] args) {
+        countNumbersFrom0To5();
+        countNumbersFrom0ToN(10); // int n = 10
+        countNumbersFrom0ToN(8); // int n = 8
+        countNumbersFrom0ToN(4); // int n = 4
+    }
+
+    public static void countNumbersFrom0To5(){
+
+        for(int i = 0; i <= 5; i++){
+            System.out.print(i + " ");
+        }
+       System.out.println();
+
+    }
+
+    public static void countNumbersFrom0ToN(int n){
+
+        for(int i = 0; i <= n; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+    }
+
 }
