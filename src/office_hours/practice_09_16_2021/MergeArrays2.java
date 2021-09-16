397b788ffad595bbbeadf105c618b9fd2aa57f6d
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-16 17:51:17 -0500

    Office hours

--- src/office_hours/practice_09_16_2021/MergeArrays2.java
+++ src/office_hours/practice_09_16_2021/MergeArrays2.java
@@ -6,8 +6,8 @@ public class MergeArrays2 {
         { 1, 2, 3 }
         { 10, 20, 30}
 
-       first case:
-        output: 1, 2, 3, 10 20, 30
+       second case:
+        output: 1, 10, 2, 20, 3, 30
 
      */
 
@@ -17,6 +17,20 @@ public class MergeArrays2 {
         int[] arrTwo = {10, 20, 30};
 
         int mergedLength = arrOne.length + arrTwo.length;
+
+        int [] merge = new int[mergedLength]; // 1, 10, 0, 0, 0, 0, 0
+
+        int j = 0;
+        for(int i = 0; i < merge.length; ){ // this loop goes through the merge array
+
+            merge[i++] = arrOne[j];
+
+            merge[i++] = arrTwo[j];
+
+
+
+        }
+
 
     }
 }
