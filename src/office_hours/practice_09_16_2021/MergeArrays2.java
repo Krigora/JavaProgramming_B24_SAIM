b2cd9347a2570cb899d8756ad770d9fabd4ee70e
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-16 17:36:00 -0500

    Office hours

---
+++ src/office_hours/practice_09_16_2021/MergeArrays2.java
@@ -0,0 +1,22 @@
+package office_hours.practice_09_16_2021;
+
+public class MergeArrays2 {
+     /*
+
+        { 1, 2, 3 }
+        { 10, 20, 30}
+
+       first case:
+        output: 1, 2, 3, 10 20, 30
+
+     */
+
+    public static void main(String[] args) {
+
+        int[] arrOne = {1, 2, 3, 4};
+        int[] arrTwo = {10, 20, 30};
+
+        int mergedLength = arrOne.length + arrTwo.length;
+
+    }
+}
