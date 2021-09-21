8e3b56f4900ef5f39f4e2548531a78c49ef77382
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-20 21:01:59 -0500

    ArrayList

--- src/day40_arraylist/RemoveByValue.java
+++ src/day40_arraylist/RemoveByValue.java
@@ -6,7 +6,20 @@ public class RemoveByValue {
 
     public static void main(String[] args) {
 
-        ArrayList<String>
+        ArrayList<String> list = new ArrayList<>();
+        list.add("pen");
+        list.add("mouse");
+        list.add("mouse");
+        list.add("marker");
+
+        boolean removed =  list.remove("mouse"); // removes the first mouse occurrence
+        System.out.println(removed);
+
+        System.out.println(list);
+
+        boolean removed2 = list.remove("hat");
+        System.out.println(removed2);
+        System.out.println(list);
 
 
     }
