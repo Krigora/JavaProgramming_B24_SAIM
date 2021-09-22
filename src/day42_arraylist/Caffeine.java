9848dcf1c3e173457e0ca8a1f74d4ab08214cfff
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-22 18:51:46 -0500

    day 42 ArrayList

---
+++ src/day42_arraylist/Caffeine.java
@@ -0,0 +1,31 @@
+package day42_arraylist;
+
+public class Caffeine {
+
+    /*
+
+        ArrayList of caffeinated drinks
+
+        Figure out how much caffeine each drink has, and store that into a new ArrayList
+
+        coffee - 80
+        tea - 50
+        monster - 100
+        red bull - 100
+        coke - 70
+        pepsi - 70
+        bang - 100
+        apple juice - 0
+
+     */
+
+    public static void main(String[] args) {
+
+        String [] allDrinks = {"coffee", "tea", "monster", "red bull", "coke", "pepsi", "bang", "apple juice"};
+
+
+
+    }
+
+
+}
