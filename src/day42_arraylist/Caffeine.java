ca208ba316787a92fed7e26579654d152cd4d1bf
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-22 19:32:15 -0500

    day 42 ArrayList

--- src/day42_arraylist/Caffeine.java
+++ src/day42_arraylist/Caffeine.java
@@ -1,5 +1,9 @@
 package day42_arraylist;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Caffeine {
 
     /*
@@ -23,9 +27,44 @@ public class Caffeine {
 
         String [] allDrinks = {"coffee", "tea", "monster", "red bull", "coke", "pepsi", "bang", "apple juice"};
 
+        ArrayList<String> drinkList = new ArrayList<>( Arrays.asList(allDrinks) );
 
+        ArrayList<Integer> caffeineAmounts = new ArrayList<>();
+
+        for(String eachDrink : drinkList){
+
+            int caffeineNum = 0;
+
+            switch (eachDrink){
+                case "apple juice":
+                    caffeineNum = 0;
+                    break;
+                case "tea":
+                    caffeineNum = 50;
+                    break;
+                case "coke":
+                case "pepsi":
+                    caffeineNum = 70;
+                    break;
+                case "coffee":
+                    caffeineNum = 80;
+                    break;
+                case "monster":
+                case "red bull":
+                case "bang":
+                    caffeineNum = 100;
+                    break;
+            }
+
+            caffeineAmounts.add(caffeineNum);
+
+        }
+
+        System.out.println(drinkList);
+        System.out.println(caffeineAmounts);
 
     }
+
 
 
 }
