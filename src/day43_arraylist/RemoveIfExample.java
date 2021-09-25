0506df792f259c8d07fbeb196a646ef9033df798
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-25 16:21:29 -0500

    day 43 ArrayList

--- src/day43_arraylist/RemoveIfExample.java
+++ src/day43_arraylist/RemoveIfExample.java
@@ -8,7 +8,8 @@ public class RemoveIfExample {
 
         ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(5, 2, 5, 2, 5, 6, 2, 6, 2, 3, 5));
 
+        nums.removeIf( n  -> n % 2 == 1  );
 
-
+        System.out.println(nums);
     }
 }
