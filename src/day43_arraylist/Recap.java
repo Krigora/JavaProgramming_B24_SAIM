e07a3f62caf455200405e622ca1c4b54f559c5a3
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-25 11:03:07 -0500

    day 43 ArrayList

--- src/day43_arraylist/Recap.java
+++ src/day43_arraylist/Recap.java
@@ -7,18 +7,25 @@ public class Recap {
 
     public static void main(String[] args) {
 
+        /*
+            The first ArrayList is made to be empty
+            then we added 3 elements using the add method 3 times
+         */
+
         ArrayList<String> first = new ArrayList<>();
         first.add("water");
         first.add("items");
         first.add("run");
 
-        ArrayList<String> second = new ArrayList<>( first );
+        // The second ArrayList object was made while copying the elements from the first ArrayList
+
+        ArrayList<String> second = new ArrayList<>( first ); // able to make ArrayList object by giving a collection type in the parenthesis
         second.add("pen");
 
         System.out.println(first);
         System.out.println(second);
 
-        ArrayList<String> third = new ArrayList<>( Arrays.asList("one", "two"));
+        ArrayList<String> third = new ArrayList<>( Arrays.asList("one", "two") );
 
         System.out.println(third);
 
