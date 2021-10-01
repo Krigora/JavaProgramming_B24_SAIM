6a9649e1996add1ad2a0b0a523ce48221b92b037
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-09-30 19:15:11 -0500

    Classes day 44

---
+++ src/day44_custom_classes/AllPeople.java
@@ -0,0 +1,22 @@
+package day44_custom_classes;
+
+public class AllPeople {
+
+    public static void main(String[] args) {
+
+        // make Person object
+
+        Person personOne = new Person(); // We made a Person object/ we made an instance of Person class
+
+        personOne.name = "James Bond";
+        personOne.age = 40;
+        //age = 40; there is no local age
+        personOne.hairColor = "Black";
+        personOne.sex = 'M';
+        personOne.isMarried = false;
+
+
+    }
+
+
+}
