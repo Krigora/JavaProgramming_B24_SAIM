073d03d275f2a228ffd19df579c7a22b208ec2ba
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-10-02 11:04:47 -0500

    Classes day 45

--- src/day45_constructors/App.java
+++ src/day45_constructors/App.java
@@ -31,9 +31,12 @@ public class App {
     boolean isFree;
 
     public void update(){
-        System.out.println(name + " of application is updating....");
-        System.out.println(name + " of application is updating....");
-        System.out.println(name + " of application is updating....");
+        System.out.println(name + " is updating....");
+        System.out.println(name + " is updating....");
+        System.out.println(name + " is updating....");
+
+        version += 0.1;
+
     }
 
 
