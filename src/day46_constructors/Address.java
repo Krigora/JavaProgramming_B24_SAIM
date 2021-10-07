2cd7f553e721c8ab5a6a85a1644449c37bcca48f
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-10-06 19:59:28 -0500

    Class custom classes

---
+++ src/day46_constructors/Address.java
@@ -0,0 +1,36 @@
+package day46_constructors;
+
+public class Address {
+
+    String street;
+    String city;
+    String state;
+    String zipcode;
+    String country;
+
+    public Address(String street, String city, String state, String zipcode){
+        street = street;
+        city = city;
+        state = state;
+        zipcode = zipcode;
+        country = "US";
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipcode='" + zipcode + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Address address = new Address("100032 Oak Drive", "Chicago", "IL", "490101");
+        System.out.println(address);
+        System.out.println(address.state);
+    }
+
+}
