75d9322edd1a04e473377c3c932cfac811894c8d
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-10-07 19:49:06 -0500

    Item class

--- src/day47_constructors/ShoppingList.java
+++ src/day47_constructors/ShoppingList.java
@@ -1,13 +1,42 @@
 package day47_constructors;
 
+import java.util.Scanner;
+
 public class ShoppingList {
 
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
 
         Item firstItem = new Item("Apples", 5, 1.33);
         firstItem.quantity += 5;
         firstItem.calculatePrice();
         System.out.println(firstItem);
+
+        String keepGoing = "yes";
+
+        while (keepGoing.equalsIgnoreCase("yes")){
+
+            System.out.println("What is the item name?");
+            String name = input.nextLine();
+
+            System.out.println("What is the quantity?");
+            int quantity = input.nextInt();
+
+            System.out.println("What is the unit price?");
+            double unitPrice = input.nextDouble();
+
+            Item item = new Item(name, quantity, unitPrice);
+            System.out.println(item);
+
+            System.out.println("Do you want to keep adding Items?");
+            input.nextLine(); // empty enter input
+
+            keepGoing = input.nextLine();
+
+        }
+
+
 
 
     }
