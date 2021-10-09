a0026b0772d665b722272f77d1413a9307d79e40
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-10-09 13:40:51 -0500

    Static weekend

---
+++ src/day48_static/CydeoStudent.java
@@ -0,0 +1,21 @@
+package day48_static;
+
+public class CydeoStudent {
+
+    String name;
+    int id;
+    int groupNumber;
+
+    static int batchNumber;
+    static String [] instructors;
+    static String schoolName;
+
+    public CydeoStudent(String name, int id, int groupNumber){
+        this.name = name;
+        this.id = id;
+        this.groupNumber = groupNumber;
+    }
+
+
+
+}
