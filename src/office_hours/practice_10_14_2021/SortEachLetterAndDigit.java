55c2d29aab15c3696a83de96b9e1410d7b36388f
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-10-14 17:40:54 -0500

    Office hours sorting task

--- src/office_hours/practice_10_14_2021/SortEachLetterAndDigit.java
+++ src/office_hours/practice_10_14_2021/SortEachLetterAndDigit.java
@@ -30,9 +30,61 @@ package office_hours.practice_10_14_2021;
  */
 
 
+import java.util.Arrays;
+
 public class SortEachLetterAndDigit {
+    public static void main(String[] args) {
+        System.out.println(sortEachPart("DC501GCCCA098911"));
+    }
+
+    public static String sortEachPart(String s){
+
+        String sub = "";  // DC
+        String sortedStr = "";
+
+        for(int i = 0; i < s.length(); i++){
+
+            char current = s.charAt(i);
+            sub += current;
+            boolean sort = false;
+
+            if(i != s.length() -1) {
+                char next = s.charAt(i + 1);
+                sort =
+                        Character.isLetter(current) && !Character.isLetter(next) ||
+                        Character.isDigit(current) && !Character.isDigit(next);
+
+            } else {
+                sort = true;
+            }
+
+            if(sort){
+                String [] toSort = sub.split("");
+                Arrays.sort(toSort);
+               // sortedStr += Arrays.toString(toSort).replace("[", "").replace("]", "").replace(", ", "");
+                sortedStr += String.join("", toSort);
+                sub = "";
+            }
+
+        }
+
+        return sortedStr;
+    }
+
+    /*
+     Input: "DC501GCCCA098911"
+     OutPut: "CD015ACCCG011899"
+     */
 
 
 }
 
+/*
 
+
+
+               boolean sort = i == s.length() - 1 ||
+                        Character.isLetter(current) && !Character.isLetter(s.charAt(i + 1)) ||
+                        Character.isDigit(current) && !Character.isDigit(s.charAt(i + 1));
+
+ */
