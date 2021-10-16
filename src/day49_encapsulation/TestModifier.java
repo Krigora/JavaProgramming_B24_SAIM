1ee91fe56f0a0dc085887a22ff5a60ac63232102
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-10-16 14:02:20 -0500

    day 49

---
+++ src/day49_encapsulation/TestModifier.java
@@ -0,0 +1,11 @@
+package day49_encapsulation;
+
+public class TestModifier {
+    public static void main(String[] args) {
+        AccessModifier obj = new AccessModifier();
+        obj.a = 1;
+        obj.b = 2;
+      //  obj.c = 3; c is private so we cannot access it outside of the class
+
+    }
+}
