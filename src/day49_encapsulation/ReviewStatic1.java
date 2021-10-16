8d47d48121eee7fe25b9c3debe3f2a59e15c71e8
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-10-16 10:01:15 -0500

    day 49

--- src/day49_encapsulation/ReviewStatic1.java
+++ src/day49_encapsulation/ReviewStatic1.java
@@ -5,27 +5,27 @@ public class ReviewStatic1 {
     static int i = 6;
     int a = 4;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // main method
         System.out.println("A");
         new ReviewStatic1();
         method1();
     }
 
-    public ReviewStatic1(){
+    public ReviewStatic1(){  // constructor
         System.out.println("B");
     }
 
-    static {
+    static {  // static block
         System.out.println("C");
-       // System.out.println(a);
+       // System.out.println(a);  cannot use instance variable in static block
     }
 
-    public static void method1(){
+    public static void method1(){ // static method
         System.out.println("D");
         System.out.println(i);
-       // System.out.println(a);
+       // System.out.println(a); // cannot use instance variable in static method
     }
-    public void method2(){
+    public void method2(){ // instance method
         System.out.println(i);
     }
 
