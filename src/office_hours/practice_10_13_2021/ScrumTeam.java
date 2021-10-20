c6fe08693b50dad3ab7806cdf9dc8c7600518b37
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-10-20 17:17:02 -0500

    Office hours Scrum Team

--- src/office_hours/practice_10_13_2021/ScrumTeam.java
+++ src/office_hours/practice_10_13_2021/ScrumTeam.java
@@ -9,6 +9,7 @@ public class ScrumTeam {
     String scrumMaster;
     int sprintNumber;
     ArrayList<Developer> allDevelopers;
+    ArrayList<Tester> allTesters;
 
     static String companyName;
     static String ceo;
@@ -16,13 +17,13 @@ public class ScrumTeam {
     public ScrumTeam(int sprintNumber) {
         this.sprintNumber = sprintNumber;
         allDevelopers = new ArrayList<>();
+        allTesters = new ArrayList<>();
     }
 
-    public ScrumTeam(String productOwner, String scrumMaster, int sprintNumber, ArrayList<Developer> allDevelopers) {
+    public ScrumTeam(String productOwner, String scrumMaster, int sprintNumber) {
         this(sprintNumber);
         this.productOwner = productOwner;
         this.scrumMaster = scrumMaster;
-        this.allDevelopers.addAll(allDevelopers);
     }
 
     public void addDeveloper(Developer developer){
@@ -33,7 +34,24 @@ public class ScrumTeam {
         allDevelopers.addAll(Arrays.asList(developers));
     }
 
+    public void addTester(Tester tester){
+        allTesters.add(tester);
+    }
 
+    public void removeTester(int employeeId){
+        allTesters.removeIf(each -> each.employeeId == employeeId);
+    }
+
+    @Override
+    public String toString() {
+        return "ScrumTeam{" +
+                "productOwner='" + productOwner + '\'' +
+                ", scrumMaster='" + scrumMaster + '\'' +
+                ", sprintNumber=" + sprintNumber +
+                ", allDevelopers=" + allDevelopers +
+                ", allTesters=" + allTesters +
+                '}';
+    }
 }
 
 
