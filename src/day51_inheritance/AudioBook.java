2a5d9b9c2b8e9ccf57a6c4ed53ea06dc4dbb2ec2
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-10-20 18:46:09 -0500

    Inheritance Book

--- src/day51_inheritance/AudioBook.java
+++ src/day51_inheritance/AudioBook.java
@@ -5,6 +5,9 @@ public class AudioBook extends Book{
     double duration;
     String narrator;
 
+    public void listen(){
+        System.out.println("Listening to " + title + " narrated by " + narrator);
+    }
 
 }
 
