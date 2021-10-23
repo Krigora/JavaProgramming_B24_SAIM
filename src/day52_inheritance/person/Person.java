ea83559ef9e5c6c35c02c85572f441723d749392
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-10-23 10:40:32 -0500

    Person example

---
+++ src/day52_inheritance/person/Person.java
@@ -0,0 +1,16 @@
+package day52_inheritance.person;
+
+import java.util.ArrayList;
+
+public class Person {
+
+    String name;
+    int age;
+    int birthYear;
+    ArrayList<String> hobbies;
+
+    public void walk(){
+        System.out.println(name + " is walking");
+    }
+
+}
---
+++ src/day52_inheritance/person/Student.java
@@ -0,0 +1,11 @@
+package day52_inheritance.person;
+
+public class Student extends Person{
+
+    String schoolName;
+
+    public void study(){
+        System.out.println(name + " is studying");
+    }
+
+}
