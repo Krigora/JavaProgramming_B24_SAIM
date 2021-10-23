1d33f024e883dfd80019ddba200f1b0b29a9a5ab
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-10-23 10:44:29 -0500

    Person example

--- src/day52_inheritance/person/Student.java
+++ src/day52_inheritance/person/Student.java
@@ -5,7 +5,7 @@ public class Student extends Person{
     String schoolName;
 
     public void study(){
-        System.out.println(name + " is studying");
+        System.out.println(name + " is studying at " + schoolName);
     }
 
 }
