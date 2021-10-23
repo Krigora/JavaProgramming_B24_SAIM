4d092b4d87e06a7dc3a9956814e44775f7fb1c56
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-10-23 13:28:57 -0500

    Computer example

---
+++ src/day52_inheritance/computer/Computer.java
@@ -0,0 +1,13 @@
+package day52_inheritance.computer;
+
+public class Computer {
+
+    String os;
+    int memory;
+
+    public Computer(String os, int memory){
+        this.os = os;
+        this.memory = memory;
+    }
+
+}
