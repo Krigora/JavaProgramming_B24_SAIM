f5a2611c8833cef4bbd4a8f3f3f9971948e985a0
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-10-27 17:33:29 -0500

    Office hours Canvas example

---
+++ src/office_hours/practice_10_27_2021/TestModule.java
@@ -0,0 +1,17 @@
+package office_hours.practice_10_27_2021;
+
+public class TestModule {
+    public static void main(String[] args) {
+
+        Module day1 = new Module();
+
+        File notes = new File("day1 notes", 0.4);
+        day1.files.add(notes);
+
+        System.out.println(day1.files.get(0));
+        day1.files.get(0).openFile();
+
+
+
+    }
+}
