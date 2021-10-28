7b39f4cc09c2e7749f388743e9e7f83cdc0230d4
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-10-27 20:58:13 -0500

    Overriding rules

---
+++ src/day53_inheritance/Child.java
@@ -0,0 +1,30 @@
+package day53_inheritance;
+
+//public class Child extends FinalExamples{  // FinalExamples cannot be a parent class
+//}
+
+
+public class Child extends Parent {
+
+//    @Override
+//    public static final void method(){
+//
+//    }
+
+    // final method cannot be overriden
+
+    public static final void method(int a){
+
+    }
+
+}
+
+class Parent{
+
+    public static final void method(){
+
+    }
+
+}
+
+
