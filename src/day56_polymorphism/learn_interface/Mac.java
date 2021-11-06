26d812827ce4a40730ad40f337c19f4c6978c413
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-11-06 12:03:40 -0500

    Day 56 interface

---
+++ src/day56_polymorphism/learn_interface/Mac.java
@@ -0,0 +1,26 @@
+package day56_polymorphism.learn_interface;
+
+public interface Mac {
+
+    public static final String NAME = "Mac";
+    String SOFTWARE = "IOS";
+
+    void turnOn(); // public abstract method
+
+    public static void getBrand(){
+        System.out.println("Apple");
+    }
+
+    public default void type(){
+        System.out.println("Typing");
+    }
+
+}
+
+class Runner2{
+    public static void main(String[] args) {
+        Mac.getBrand();
+        System.out.println(Mac.NAME);
+        System.out.println(Mac.SOFTWARE);
+    }
+}
