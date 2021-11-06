ee1dc41d05e038e9cba5899b308b2568533c1e3c
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-11-06 15:02:08 -0500

    Day 56  polymorphism

--- src/day56_polymorphism/animal/Lizard.java
+++ src/day56_polymorphism/animal/Lizard.java
@@ -7,3 +7,25 @@ public class Lizard extends Reptile {
         System.out.println("Lizard is eating -- FROM CHILD");
     }
 }
+
+class Wild {
+
+    public static void main(String[] args) {
+
+        // Make your Lizard object with all references
+
+        Lizard obj1 = new Lizard();
+
+        Reptile obj2 = new Lizard();
+
+        Animal obj3 = new Lizard();
+
+        obj1.eat();
+        obj2.eat();
+        obj3.eat();
+
+
+    }
+
+
+}
