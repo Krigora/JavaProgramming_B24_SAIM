80521e402c77d2924c1ef787531f10a148c3449c
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-11-20 12:08:35 -0600

    Collection framework

---
+++ src/day60_collections/ListObjects.java
@@ -0,0 +1,22 @@
+package day60_collections;
+
+import day53_inheritance.rules.A;
+
+import java.util.ArrayList;
+
+public class ListObjects {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("c");
+        list.add(null);
+        System.out.println(list);
+
+
+
+
+    }
+}
