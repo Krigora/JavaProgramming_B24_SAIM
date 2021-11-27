0fe5ac43e6fc12c2ec107e6e8e6e7478936788a4
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-11-27 13:31:44 -0600

    Maps

---
+++ src/day62_maps/MapClasses.java
@@ -0,0 +1,19 @@
+package day62_maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapClasses {
+    public static void main(String[] args) {
+
+        Map<Integer, String> map1 = new HashMap<>();
+        map1.put(2, "two");
+        map1.put(4, "four");
+        map1.put(1, "one");
+        map1.put(5, "five");
+        map1.put(3, "three");
+
+
+
+    }
+}
