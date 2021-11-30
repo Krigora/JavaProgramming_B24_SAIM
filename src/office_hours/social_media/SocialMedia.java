b225c024e2251ae5c89c7ad282a5e6c66c133df7
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-11-30 16:50:41 -0600

    Office hours Social Media

---
+++ src/office_hours/social_media/SocialMedia.java
@@ -0,0 +1,26 @@
+package office_hours.social_media;
+
+public abstract class SocialMedia {
+
+    public String personalUrl;
+    public int accountLength;
+    public static String platform;
+
+    public abstract void directMessage(String username, String message);
+    public abstract void post(String body);
+    public abstract void notification();
+
+}
+
+
+/*
+
+Social Media
+• Create an abstract class for Social Media that has the following features:
+- Direct messaging(String username, String message) - Post(String body)
+- Notifications()
+• The Social Media will also have the following fields:
+- Personal URl (String) - Account length (int)
+- Platform (static String)
+
+ */
